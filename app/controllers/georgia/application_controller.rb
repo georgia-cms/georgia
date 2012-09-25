b673c4d020e0ef1fe83675af22d8e176192af34d
Mathieu Gagné <mathieu@orangebrule.com>
2012-09-25 11:01:46 -0400

    Added down_arrow.gif

--- app/controllers/georgia/application_controller.rb
+++ app/controllers/georgia/application_controller.rb
@@ -1,25 +1,25 @@
 module Georgia
-	class ApplicationController < ActionController::Base
+  class ApplicationController < ActionController::Base
 
-		# Required otherwise get the error 'uninitialized Ability'
-		# require 'devise'
+    # Required otherwise get the error 'uninitialized Ability'
+    # require 'devise'
 
-		helper 'georgia/ui'
-		helper 'georgia/internationalization'
-		helper 'georgia/form_actions'
-		helper 'georgia/routes'
+    helper 'georgia/ui'
+    helper 'georgia/internationalization'
+    helper 'georgia/form_actions'
+    helper 'georgia/routes'
 
-		protect_from_forgery
-		
-		before_filter :authenticate_user!, except: :login
-		
-		def login
-			render 'users/sessions/new'
-		end
+    protect_from_forgery
+    
+    before_filter :authenticate_user!, except: :login
+    
+    def login
+      render 'users/sessions/new'
+    end
 
-		def current_ability
-			@current_ability ||= Georgia::Ability.new(current_user)
-		end
-		
-	end
+    def current_ability
+      @current_ability ||= Georgia::Ability.new(current_user)
+    end
+    
+  end
 end
