7fc7d59bccb26729f984f6e305242d864cc21261
Mathieu Gagné <mathieu@orangebrule.com>
2012-10-03 16:35:46 -0400

    Namespaced form actions helper urls

--- app/helpers/georgia/form_actions_helper.rb
+++ app/helpers/georgia/form_actions_helper.rb
@@ -1,6 +1,6 @@
 module Georgia
   module FormActionsHelper
-    
+
     def sortable(column, title = nil)
       title ||= column.titleize
       css_class = column == sort_column ? "current #{sort_direction}" : nil
@@ -15,21 +15,21 @@ module Georgia
     def link_to_publish(model)
       return if model.nil? or model.new_record?
       if can? :publish, model
-        link_to "#{icon_tag('icon-thumbs-up')} Publish".html_safe, [:publish, model]
+        link_to "#{icon_tag('icon-thumbs-up')} Publish".html_safe, namespaced_url_for(model, :publish)
       end
     end
 
     def link_to_unpublish(model)
       return if model.nil? or model.new_record?
       if can? :unpublish, model
-        link_to "#{icon_tag('icon-thumbs-down')} Unpublish".html_safe, [:unpublish, model]
+        link_to "#{icon_tag('icon-thumbs-down')} Unpublish".html_safe, namespaced_url_for(model, :unpublish)
       end
     end
 
     def link_to_review(model)
       return if model.nil? or model.new_record?
       if can? :ask_for_review, model
-        link_to "#{icon_tag('icon-flag')} Ask for Review".html_safe, [:ask_for_review, model]
+        link_to "#{icon_tag('icon-flag')} Ask for Review".html_safe, namespaced_url_for(model, :ask_for_review)
       end
     end
 
@@ -45,39 +45,14 @@ module Georgia
     def link_to_delete(model)
       return if model.nil? or model.new_record?
       if can? :destroy, model
-        link_to "#{icon_tag('icon-trash')} Delete".html_safe, [model], data: {confirm: 'Are you sure?'}, method: :delete
+        link_to "#{icon_tag('icon-trash')} Delete".html_safe, namespaced_url_for(model), data: {confirm: 'Are you sure?'}, method: :delete
       end
-    end
-
-    def link_to_remove_fields(name, f, options={})
-      f.hidden_field(:_destroy) + link_to_function(name, "FormHelper.remove_fields(this)", options)
-    end
-
-    def link_to_remove_slide(name, f, options={})
-      return unless f and f.object and !f.object.new_record?
-      f.hidden_field(:_destroy) + link_to_function(name, "FormHelper.remove_slide(this)", options)
-    end
-
-    def link_to_add_fields(name, f, association)
-      new_object = f.object.class.reflect_on_association(association).klass.new
-      fields = f.fields_for(association, new_object, child_index: "new_#{association}") do |builder|
-        render("shared/" + association.to_s.singularize, f: builder)
-      end
-      link_to_function(raw("<i class='icon-plus-sign'></i> " + name), "FormHelper.add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", class: 'btn')
-    end
-
-    def link_to_add_fields_row(name, f, association)
-      new_object = f.object.class.reflect_on_association(association).klass.new
-      fields = f.fields_for(association, new_object, child_index: "new_#{association}") do |builder|
-        render("shared/" + association.to_s.singularize, f: builder)
-      end
-      link_to_function(raw("<i class='icon-plus-sign'></i> " + name), "FormHelper.add_fields_row(this, \"#{association}\", \"#{escape_javascript(fields)}\")", class: 'btn')
     end
 
     protected
 
     def link_to_group_button(model)
-      link_to(icon_tag('icon-cog'), [:edit, model], class: 'btn btn-inverse') +
+      link_to(icon_tag('icon-cog'), namespaced_url_for(model, :edit), class: 'btn btn-inverse') +
       link_to(content_tag('span', '', class: 'caret'), '#', class: 'btn btn-inverse dropdown-toggle', data: {toggle: "dropdown"})
     end
 
@@ -92,28 +67,24 @@ module Georgia
     end
 
     def link_to_group_item_publish(model, options={})
-      return unless model
-      link_to "#{icon_tag('icon-ok')} Publish".html_safe, [:publish, model] if can? :publish, model
+      link_to "#{icon_tag('icon-ok')} Publish".html_safe, namespaced_url_for(model, :publish) if can? :publish, model
     end
 
     def link_to_group_item_unpublish(model, options={})
-      return unless model
-      link_to "#{icon_tag('icon-ban-circle')} Unpublish".html_safe, [:unpublish, model] if can? :unpublish, model
+      link_to "#{icon_tag('icon-ban-circle')} Unpublish".html_safe, namespaced_url_for(model, :unpublish) if can? :unpublish, model
     end
 
     def link_to_group_item_edit(model, options={})
-      return unless model
-      link_to "#{icon_tag('icon-pencil')} Edit".html_safe, [:edit, model] if can? :edit, model
+      link_to "#{icon_tag('icon-pencil')} Edit".html_safe, namespaced_url_for(model, :edit) if can? :edit, model
     end
 
     def link_to_group_item_delete(model, options={})
-      return unless model
-      link_to "#{icon_tag('icon-trash')} Delete".html_safe, [model], data: {confirm: 'Are you sure?'}, method: :delete if can? :destroy, model
+      link_to "#{icon_tag('icon-trash')} Delete".html_safe, namespaced_url_for(model), data: {confirm: 'Are you sure?'}, method: :delete if can? :destroy, model
     end
 
     def link_to_group_item_ask_for_review(model, options={})
-      return unless model
-      link_to "#{icon_tag('icon-check')} Ask for review".html_safe, [:ask_for_review, model] if can? :ask_for_review, model
+      link_to "#{icon_tag('icon-check')} Ask for review".html_safe, namespaced_url_for(model, :ask_for_review) if can? :ask_for_review, model
     end
+
   end
 end
--- app/helpers/georgia/routes_helper.rb
+++ app/helpers/georgia/routes_helper.rb
@@ -9,5 +9,22 @@ module Georgia
       'http://www.motioneleven.com'
     end
 
+    def namespaced_url_for(model, action=nil)
+      case namespace
+      when 'Georgia'
+        georgia.url_for([action, model].compact)
+      when 'Admin'
+        main_app.url_for([action, :admin, model].compact)
+      else
+        main_app.url_for([action, model].compact)
+      end
+    end
+
+    protected
+
+    def namespace
+      @namespace ||= controller.class.to_s.split('::').first
+    end
+
   end
 end
