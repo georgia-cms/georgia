60cfd95aecb33467ce596a38a575bcfe7a0cdbfd
Mathieu Gagné <mathieu@motioneleven.com>
2012-11-01 16:25:10 -0400

    Limit excerpt and title to 255 chars

--- app/models/georgia/content.rb
+++ app/models/georgia/content.rb
@@ -2,7 +2,9 @@ module Georgia
   class Content < ActiveRecord::Base
     attr_accessible :published_at, :title, :text, :excerpt, :keywords, :locale, :image_id
 
+    validates :title, length: {maximum: 255}
     validates :keywords, length: {maximum: 255}
+    validates :excerpt, length: {maximum: 255}
 
     belongs_to :contentable, polymorphic: true
 
