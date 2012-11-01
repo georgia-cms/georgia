941a787df05520bce97daff066a0b1b8adf19cbf
Mathieu Gagné <mathieu@motioneleven.com>
2012-11-01 15:22:05 -0400

    Remove localized url link

--- app/decorators/georgia/link_decorator.rb
+++ app/decorators/georgia/link_decorator.rb
@@ -4,13 +4,7 @@ module Georgia
     decorates_association :page, with: Georgia::PageDecorator
 
     def url
-      if text.match(/^http/)
-        text
-      else
-        @url = text
-        @url.insert(0, "/#{I18n.locale}") if I18n.available_locales.length > 1
-        @url
-      end
+      text
     end
   end
 end
