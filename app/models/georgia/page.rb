b4819be97022348867149262f91763d31c185434
Mathieu Gagné <mathieu@motioneleven.com>
2013-01-04 17:58:56 -0500

    Add ordered scope & remove limit for featured image

--- app/models/georgia/page.rb
+++ app/models/georgia/page.rb
@@ -35,10 +35,12 @@ module Georgia
 
     include PgSearch
     pg_search_scope :text_search, using: {tsearch: {dictionary: 'english', prefix: true, any_word: true}},
+      against: [:slug],
       associated_against: { contents: [:title, :text, :excerpt, :keywords] }
 
     default_scope includes(:contents)
     scope :published, joins(:status).where('georgia_statuses' => {name: Georgia::Status::PUBLISHED})
+    scope :ordered, order(:position)
 
     before_save :ensure_status
     before_validation :sanitize_slug
