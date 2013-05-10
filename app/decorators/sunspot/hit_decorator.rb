ebbf500310e953946f2245c301f9656e1b0bdd9e
Mathieu Gagné <mathieu@motioneleven.com>
2013-05-10 16:20:31 -0400

    Add Sunspot::HitDecorator

---
+++ app/decorators/sunspot/hit_decorator.rb
@@ -0,0 +1,27 @@
+module Sunspot
+  class HighlightedHitDecorator < ApplicationDecorator
+
+    def method_missing(meth, *args, &block)
+      if source.highlights(meth) and source.highlights(meth).any?
+        highlight_text_on(meth)
+      elsif source.stored(meth) and source.stored(meth).any?
+        source.stored(meth).first
+      elsif source.result
+        source.result.send(meth)
+      else
+        super
+      end
+    end
+
+    def source_class_name
+      source.class_name.parameterize
+    end
+
+    private
+
+    def highlight_text_on key, options={}
+      source.highlights(key).map{|hl| hl.format{|word| h.content_tag(:span, word, class: 'highlight') } }.join().html_safe
+    end
+
+  end
+end
