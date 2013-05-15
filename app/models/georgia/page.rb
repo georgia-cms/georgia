f9401ed6314c74ddcfc630389cb65f3aa3d46397
Mathieu Gagné <mathieu@motioneleven.com>
2013-05-15 15:30:39 -0400

    Add Georgia::Concerns::Indexable

--- app/models/georgia/page.rb
+++ app/models/georgia/page.rb
@@ -8,6 +8,7 @@ module Georgia
     include Concerns::Taggable
     include Concerns::Templatable
     include Concerns::Orderable
+    include Concerns::Indexable
 
     acts_as_list scope: :parent #override orderable to include scope
 
@@ -29,41 +30,6 @@ module Georgia
     has_many :widgets, through: :ui_associations
 
     scope :not_self, ->(page) {where('georgia_pages.id != ?', page.id)}
-
-    searchable do
-      text :title, stored: true do
-        contents.map(&:title).join(', ')
-      end
-      text :excerpt, stored: true do
-        contents.map(&:excerpt).join(', ')
-      end
-      text :text do
-        contents.map(&:text).join(', ')
-      end
-      text :keywords do
-        contents.map(&:keyword_list).flatten.join(', ')
-      end
-      text :tags do
-        tag_list.join(', ')
-      end
-      text :url
-      text :template
-      text :status_name
-      string :type
-      string :title
-      string :excerpt
-      string :text
-      string :url
-      string :template
-      string :status_name
-      string :keywords, stored: true, multiple: true do
-        contents.map(&:keyword_list).flatten
-      end
-      string :tag_list, stored: true, multiple: true #Facets (multiple)
-      string :tags do #Ordering (single list)
-        tag_list.join(', ')
-      end
-    end
 
   end
 end
