97d257f25b2bbfd26c3316cc988b28cfafc01fb7
Mathieu Gagné <mathieu@motioneleven.com>
2013-05-24 10:21:28 -0400

    Fix same migration number error

--- lib/generators/georgia/upgrade/upgrade_generator.rb
+++ lib/generators/georgia/upgrade/upgrade_generator.rb
@@ -1,16 +1,18 @@
 # encoding: UTF-8
 require 'rails/generators/migration'
+require 'rails/generators/active_record'
 
 module Georgia
   module Generators
     class UpgradeGenerator < ::Rails::Generators::Base
       include Rails::Generators::Migration
+
       source_root File.expand_path('../templates', __FILE__)
 
       desc "Upgrades Georgia CMS to fit latest changes"
 
-      def self.next_migration_number(dirname)
-        Time.now.strftime("%Y%m%d%H%M%S")
+      def self.next_migration_number(number)
+        ActiveRecord::Generators::Base.next_migration_number(number)
       end
 
       def create_migration
