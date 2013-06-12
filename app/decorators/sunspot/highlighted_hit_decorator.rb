ad45c8062ad6d96be645a0fcc824efa15dfa1784
Mathieu Gagné <mathieu@motioneleven.com>
2013-06-12 12:45:42 -0400

    Sunspot decorator to give joined array instead of first value

--- app/decorators/sunspot/highlighted_hit_decorator.rb
+++ app/decorators/sunspot/highlighted_hit_decorator.rb
@@ -2,10 +2,12 @@ module Sunspot
   class HighlightedHitDecorator < ::ApplicationDecorator
 
     def method_missing(meth, *args, &block)
-      if source.highlights(meth) and source.highlights(meth).any?
+      if source.highlights(meth) and source.highlights(meth).present?
         highlight_text_on(meth)
-      elsif source.stored(meth) and source.stored(meth).any?
-        source.stored(meth).first
+      elsif source.stored(meth) and source.stored(meth).is_a? Array
+        source.stored(meth).join(', ')
+      elsif source.stored(meth) and source.stored(meth).present?
+        source.stored(meth)
       elsif source.result
         source.result.send(meth)
       else
