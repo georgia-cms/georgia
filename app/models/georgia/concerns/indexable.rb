7e0b64930948b1b4401a23065b046352435aa958
Mathieu Gagné <mathieu@motioneleven.com>
2013-06-13 12:28:40 -0400

    Move searchable block to indexable_fields

--- app/models/georgia/concerns/indexable.rb
+++ app/models/georgia/concerns/indexable.rb
@@ -7,39 +7,51 @@ module Georgia
 
       included do
 
+        class << self
+
+          # Default fields to be include in searchable block
+          # Children of Georgia::Page can use this to extend their own searchable block
+          def indexable_fields
+            Proc.new {
+              text :title, stored: true do
+                contents.map(&:title).join(', ')
+              end
+              text :excerpt, stored: true do
+                contents.map(&:excerpt).join(', ')
+              end
+              text :text do
+                contents.map(&:text).join(', ')
+              end
+              text :keywords do
+                contents.map(&:keyword_list).flatten.join(', ')
+              end
+              text :tags do
+                tag_list.join(', ')
+              end
+              text :url
+              text :template
+              text :status_name
+              string :type
+              string :title
+              string :excerpt
+              string :text
+              string :url
+              string :template
+              string :status_name
+              string :keywords, stored: true, multiple: true do
+                contents.map(&:keyword_list).flatten
+              end
+              string :tag_list, stored: true, multiple: true #Facets (multiple)
+              string :tags do #Ordering (single list)
+                tag_list.join(', ')
+              end
+            }
+          end
+
+        end
+
         searchable do
-          text :title, stored: true do
-            contents.map(&:title).join(', ')
-          end
-          text :excerpt, stored: true do
-            contents.map(&:excerpt).join(', ')
-          end
-          text :text do
-            contents.map(&:text).join(', ')
-          end
-          text :keywords do
-            contents.map(&:keyword_list).flatten.join(', ')
-          end
-          text :tags do
-            tag_list.join(', ')
-          end
-          text :url
-          text :template
-          text :status_name
-          string :type
-          string :title
-          string :excerpt
-          string :text
-          string :url
-          string :template
-          string :status_name
-          string :keywords, stored: true, multiple: true do
-            contents.map(&:keyword_list).flatten
-          end
-          string :tag_list, stored: true, multiple: true #Facets (multiple)
-          string :tags do #Ordering (single list)
-            tag_list.join(', ')
-          end
+          instance_eval &Georgia::Page.indexable_fields
         end
 
       end
