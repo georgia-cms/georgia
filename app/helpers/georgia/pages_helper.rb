450d8e18f0d9b2b18bdeda634456ac36cfbde248
Alexandre Croteau-Pothier <alex@motioneleven.com>
2013-06-27 15:55:56 -0400

    added a comment + fixme to a hard to understand method

--- app/helpers/georgia/pages_helper.rb
+++ app/helpers/georgia/pages_helper.rb
@@ -1,7 +1,12 @@
 module Georgia
   module PagesHelper
 
-    # This needs improvement
+    # FIXME: This needs improvement
+		# This methods allow overriding of partials
+		#
+		# For example to override view/georgia/pages/fields/_content.html.erb
+		# in Admin::ThingsController
+		# Simply create view/admin/things/fields/_content.html.erb
     def render_partial_or_default view, controller=nil, locals
       render view, locals
     rescue ActionView::MissingTemplate
