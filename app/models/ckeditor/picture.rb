ad96f38fa08326f044c80985fe8630dc50a2a89f
Mathieu Gagné <mathieu@motioneleven.com>
2013-07-15 10:07:25 -0400

    Add featured and featured count to media library on pictures

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -2,4 +2,14 @@ class Ckeditor::Picture < Ckeditor::Asset
 
   mount_uploader :data, CkeditorPictureUploader, mount_on: :data_file_name
 
+  has_many :contents, foreign_key: :image_id, class_name: Georgia::Content
+
+  def featured?
+    @featured ||= contents.any?
+  end
+
+  def featured_count
+    @featured_count ||= contents.count
+  end
+
 end
