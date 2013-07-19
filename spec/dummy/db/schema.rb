e7591893f434d5675344b1e9ecc36e93ab52ca11
Mathieu Gagne <mathieu@motioneleven.com>
2013-07-19 17:43:30 -0400

    Reset dummy app

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130719090529) do
+ActiveRecord::Schema.define(:version => 20130719173129) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
