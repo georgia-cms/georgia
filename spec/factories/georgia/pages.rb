8723fbbbe94adbf4a13a129f4050dbbad0f2f3c4
Mathieu Gagné <mathieu@motioneleven.com>
2013-07-19 15:27:25 -0400

    Remove status on page factory

--- spec/factories/georgia/pages.rb
+++ spec/factories/georgia/pages.rb
@@ -2,6 +2,5 @@ FactoryGirl.define do
   factory :georgia_page, class: Georgia::Page do
     template 'one-column'
     sequence(:slug) {|n| "page#{n}"}
-    association :status, factory: :georgia_status
   end
 end
