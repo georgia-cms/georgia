837731f17505c74e15972ae4323abaf88baaac3e
Mathieu Gagné <mathieu@motioneleven.com>
2013-07-19 15:23:54 -0400

    Add indentation

--- app/models/georgia/concerns/slugable.rb
+++ app/models/georgia/concerns/slugable.rb
@@ -9,7 +9,9 @@ module Georgia
 
         attr_accessible :slug, :url
 
-        validates :slug, format: {with: /^[a-zA-Z0-9_-]+$/, message: 'can only consist of letters, numbers, dash (-) and underscore (_)'}, uniqueness: {scope: :ancestry, message: 'has already been taken'}
+        validates :slug,
+          format:     {with: /^[a-zA-Z0-9_-]+$/,  message: 'can only consist of letters, numbers, dash (-) and underscore (_)'},
+          uniqueness: {scope: :ancestry, message: 'has already been taken'}
 
         before_validation :sanitize_slug
         after_save :update_url
