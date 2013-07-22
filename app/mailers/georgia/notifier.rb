3942415a272cd67e04018e132c128456756d5ff7
Mathieu Gagne <mathieu@motioneleven.com>
2013-07-21 22:46:40 -0400

    Do not send emails in dev

--- app/mailers/georgia/notifier.rb
+++ app/mailers/georgia/notifier.rb
@@ -13,22 +13,26 @@ module Georgia
     end
 
     def notify_admins(message, url)
+      return if Rails.env.development?
       @message = message
       @url = url
       domain_name = Rails.application.config.action_mailer.smtp_settings[:domain]
-      emails_to = (Rails.env.development? ? 'dev@motioneleven.com' : Georgia::User.admins.map(&:email))
-      mail(
-        from: "do-not-reply@#{domain_name}",
-        to: emails_to,
-        subject: @message
-        )
+      emails_to = Georgia::User.admins.map(&:email)
+      unless emails_to.empty?
+        mail(
+          from: "do-not-reply@#{domain_name}",
+          to: emails_to,
+          subject: @message
+          )
+      end
     end
 
     def notify_editors(message, url)
+      return if Rails.env.development?
       @message = message
       @url = url
       domain_name = Rails.application.config.action_mailer.smtp_settings[:domain]
-      emails_to = (Rails.env.development? ? 'dev@motioneleven.com' : Georgia::User.editors.map(&:email))
+      emails_to = Georgia::User.editors.map(&:email)
       unless emails_to.empty?
         mail(
           from: "do-not-reply@#{domain_name}",
