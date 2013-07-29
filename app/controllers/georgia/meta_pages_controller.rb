53c855a6f0b6d3b789628fa08c0970f00cd85612
Mathieu Gagné <mathieu@motioneleven.com>
2013-07-29 15:35:39 -0400

    Move Publishable concern back to pages controller

--- app/controllers/georgia/meta_pages_controller.rb
+++ app/controllers/georgia/meta_pages_controller.rb
@@ -3,8 +3,6 @@ module Georgia
 
     load_and_authorize_resource class: Georgia::MetaPage
 
-    include Georgia::Concerns::Publishable
-
     def details
       @publisher = Georgia::Publisher.new(params[:id])
       @page = Georgia::PageDecorator.decorate(@publisher.meta_page)
--- app/controllers/georgia/pages_controller.rb
+++ app/controllers/georgia/pages_controller.rb
@@ -2,6 +2,7 @@ module Georgia
   class PagesController < Georgia::ApplicationController
 
     include Georgia::Concerns::Pageable
+    include Georgia::Concerns::Publishable
     include Georgia::Concerns::Searchable
 
   end
