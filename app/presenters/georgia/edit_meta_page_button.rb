116d4de50c3834c372491ac7ae8800d30e4cee62
Mathieu Gagné <mathieu@motioneleven.com>
2013-07-30 12:09:33 -0400

    Use EditPageActionList for EditPageButton

--- app/presenters/georgia/edit_meta_page_button.rb
+++ app/presenters/georgia/edit_meta_page_button.rb
@@ -1,21 +1,15 @@
 module Georgia
   class EditMetaPageButton < EditPageButton
 
+    def group_list
+      html = ""
+      html << EditMetaPageActionList.new(view, instance).render
+      content_tag 'ul', html.html_safe, class: 'dropdown-menu pull-right'
+    end
+
     def group_button
       link_to(icon_tag('icon-dashboard'), [:details, instance], class: 'btn btn-inverse') +
       link_to(content_tag('span', '', class: 'caret'), '#', group_button_options)
-    end
-
-    def group_item_delete
-      link_to "#{icon_tag('icon-trash')} Delete".html_safe, instance, data: {confirm: 'Are you sure?'}, method: :delete if can? :destroy, instance
-    end
-
-    def group_item_publish
-      link_to "#{icon_tag('icon-ok')} Publish".html_safe, [:publish, instance]
-    end
-
-    def group_item_unpublish
-      link_to "#{icon_tag('icon-ban-circle')} Unpublish".html_safe, [:unpublish, instance]
     end
 
   end
--- app/presenters/georgia/edit_page_button.rb
+++ app/presenters/georgia/edit_page_button.rb
@@ -30,47 +30,13 @@ module Georgia
 
     def group_list
       html = ""
-      html << content_tag('li', group_item_edit) if actions[:edit]
-      html << content_tag('li', group_item_preview) if actions[:preview]
-      html << content_tag('li', group_item_copy) if actions[:copy]
-      html << content_tag('li', group_item_store) if actions[:store]
-      html << content_tag('li', group_item_publish) if actions[:publish]
-      html << content_tag('li', group_item_unpublish) if actions[:unpublish]
-      html << content_tag('li', group_item_review) if actions[:review]
-      html << content_tag('li', group_item_delete) if actions[:delete]
-      content_tag 'ul', html.html_safe, class: 'dropdown-menu pull-right'
+      html << content_tag(:li, link_to_edit)
+      html << EditPageActionList.new(view, instance).render
+      content_tag :ul, html.html_safe, class: 'dropdown-menu pull-right'
     end
 
-    def group_item_copy
-      link_to "#{icon_tag('icon-copy')} Copy".html_safe, url_for(controller: controller_name, id: instance.id, action: :copy)
-    end
-
-    def group_item_store
-      link_to "#{icon_tag('icon-download')} Store".html_safe, url_for(controller: controller_name, id: instance.id, action: :store)
-    end
-
-    def group_item_preview
-      link_to("#{icon_tag('icon-eye-open')} Preview".html_safe, instance.preview_url, target: '_blank')
-    end
-
-    def group_item_publish
-      link_to "#{icon_tag('icon-ok')} Publish".html_safe, [:publish, meta_page, instance]
-    end
-
-    def group_item_unpublish
-      link_to "#{icon_tag('icon-ban-circle')} Unpublish".html_safe, [:unpublish, meta_page, instance]
-    end
-
-    def group_item_edit
-      link_to "#{icon_tag('icon-pencil')} Edit".html_safe, [:edit, meta_page, instance]
-    end
-
-    def group_item_delete
-      link_to "#{icon_tag('icon-trash')} Delete".html_safe, [meta_page, instance], data: {confirm: 'Are you sure?'}, method: :delete
-    end
-
-    def group_item_review
-      link_to "#{icon_tag('icon-check')} Ask for review".html_safe, [:review, meta_page, instance]
+    def link_to_edit
+      link_to "#{icon_tag('icon-pencil')} Edit".html_safe, [:edit, meta_page, instance] if can?(:edit, instance)
     end
 
     def group_button_options
