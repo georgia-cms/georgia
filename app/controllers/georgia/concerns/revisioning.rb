9ecd7459e5872f35f8a9dcc8d8015d01339a512c
Mathieu Gagné <mathieu@motioneleven.com>
2013-08-07 10:24:55 -0400

    Add revisioning concern

---
+++ app/controllers/georgia/concerns/revisioning.rb
@@ -0,0 +1,55 @@
+require 'active_support/concern'
+
+module Georgia
+  module Concerns
+    module Revisioning
+      extend ActiveSupport::Concern
+      include Helpers
+
+      included do
+
+        before_filter :prepare_revision, only: [:review, :approve, :decline, :revert]
+
+        def review
+          @revision.review
+          notify("#{current_user.name} is asking you to review #{@page.title} #{instance_name}.")
+          redirect_to [:edit, @page, @revision], notice: "You successfully submited #{@review.title} for review."
+        end
+
+        def approve
+          @revision.approve
+          message = "#{current_user.name} has successfully approved and published #{@page.title} #{instance_name}."
+          notify(message)
+          redirect_to [:show, @page], notice: message
+        end
+
+        def decline
+          @revision.decline
+          message = "#{current_user.name} has successfully published #{@page.title} #{instance_name}."
+          notify(message)
+          redirect_to [:edit, @page, @revision], notice: message
+        end
+
+        def revert
+          @revision.revert
+          message = "#{current_user.name} has successfully published #{@page.title} #{instance_name}."
+          notify(message)
+          redirect_to [:show, @page], notice: message
+        end
+
+        private
+
+        def prepare_revision
+          @page = Georgia::Page.find(params[:page_id])
+          @revision = Georgia::Revision.find(params[:id])
+        end
+
+        def notify(message)
+          Notifier.notify_editors(message, url_for(action: :edit, controller: controller_name, id: @page.id)).deliver
+        end
+
+      end
+
+    end
+  end
+end
