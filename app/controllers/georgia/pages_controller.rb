165e7837bfae33060548d25a716ca9489d983205
Mathieu Gagné <mathieu@motioneleven.com>
2013-08-07 10:19:21 -0400

    Move publishable to pageable

--- app/controllers/georgia/pages_controller.rb
+++ app/controllers/georgia/pages_controller.rb
@@ -2,7 +2,6 @@ module Georgia
   class PagesController < Georgia::ApplicationController
 
     include Georgia::Concerns::Pageable
-    include Georgia::Concerns::Publishable
     include Georgia::Concerns::Searchable
 
   end
