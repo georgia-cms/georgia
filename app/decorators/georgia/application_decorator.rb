cf7348c2800f692ff3c67ec08a6429a34b8cb443
Mathieu Gagné <mathieu@motioneleven.com>
2013-08-07 10:36:58 -0400

    Remove timestamps and created_by methods

--- app/decorators/georgia/application_decorator.rb
+++ app/decorators/georgia/application_decorator.rb
@@ -1,21 +1,6 @@
 module Georgia
   class ApplicationDecorator < Draper::Decorator
-
     delegate_all
-
-    %w(created_at updated_at).each do |timestamp|
-      define_method("pretty_#{timestamp}") { model.send(timestamp).strftime('%F') }
-    end
-
-    def created_by_name
-      return 'unknown' unless model and model.created_by
-      "#{model.created_by.name} (#{pretty_created_at})"
-    end
-
-    def updated_by_name
-      return 'unknown' unless model and model.updated_by
-      "#{model.updated_by.name} (#{pretty_updated_at})"
-    end
 
   end
 end
