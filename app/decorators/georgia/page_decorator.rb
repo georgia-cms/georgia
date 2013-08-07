635da1981893a7c9702439ab5875aea60c015ac6
Mathieu Gagné <mathieu@motioneleven.com>
2013-08-07 10:37:18 -0400

    Delegate to current_revision

--- app/decorators/georgia/page_decorator.rb
+++ app/decorators/georgia/page_decorator.rb
@@ -1,34 +1,11 @@
 module Georgia
   class PageDecorator < Georgia::ApplicationDecorator
+    decorates_association :current_revision
 
-    def excerpt_or_text
-      if content.excerpt and !content.excerpt.blank?
-        h.raw(content.excerpt)
-      elsif content.text and !content.text.blank?
-        h.truncate(h.strip_tags(content.text), length: 255, separator: ' ').html_safe
-      end
-    end
-
-    def status_tag(options={})
-      options[:class] ||= ''
-      options[:class] << ' label'
-      if model.is_a? Georgia::MetaPage
-        options[:class] << " label-#{human_state_name}"
-        h.content_tag(:span, human_state_name, options)
-      else
-        options[:class] << " label-#{non_namespaced_class_name}"
-        h.content_tag(:span, non_namespaced_class_name, options)
-      end
-    end
+    delegate :status_tag, :excerpt_or_text, to: :current_revision
 
     def template_path
-      "pages/templates/#{model.template}"
-    end
-
-    private
-
-    def non_namespaced_class_name
-      @non_namespaced_class_name ||= model.class.to_s.underscore.gsub(/.*\/(.*)/, '\1')
+      "pages/templates/#{current_revision.template}"
     end
 
   end
