e5bd5e7a97a7c2715b13cb111d62786a133ccd7b
Mathieu Gagné <mathieu@motioneleven.com>
2013-08-07 10:38:48 -0400

    Remove clone_as and clone in favor of draft

--- app/models/georgia/clone.rb
+++ app/models/georgia/clone.rb
@@ -4,103 +4,81 @@ module Georgia
     attr_accessor :instance, :duplicate
 
     def initialize instance
-      raise 'Instance must be persisted to be cloned' if instance.new_record?
+      raise 'Instance must be persisted to be copied' if instance.new_record?
       @instance = instance
-      @duplicate = instance.dup
-    end
-
-    # Returns an exact persisted version of itself with all Georgia::Page associations
-    def clone
-      clone_with_associations
-      duplicate
-    end
-
-    # Returns an exact persisted version of itself with all Georgia::Page associations
-    # The result is altered to return an object of a different class supported by STI
-    def clone_as klass
-      duplicate.update_attribute(:type, klass.to_s)
-      clone
-      Sunspot.index! duplicate
-      duplicate.becomes(klass)
+      @duplicate = instance.class.new
     end
 
     # Returns an exact persisted version of itself with all Georgia::Page associations
     # The resulting instance has a '(Copy)' title and '-copy' slug
     def copy
-      alter_uuid
+      copy_page
+      duplicate.current_revision = clone_current_revision
       alter_slug
-      clone
       alter_title
       duplicate.save!
       duplicate
     end
 
+    def draft
+      clone_current_revision
+    end
+
     private
 
-    def clone_with_associations
-      # Gives an ID to store associations
-      duplicate.save
+    def copy_page
+      duplicate.ancestry = instance.ancestry
+      duplicate.tag_list = instance.tag_list
+    end
 
-      # clone associations
-      clone_tags
-      clone_contents
-      clone_ui_associations
-      clone_slides
+    def clone_current_revision
+      @duplicate_revision = Georgia::Revision.create(
+        template: instance.current_revision.template,
+        state: 'draft'
+      )
+      @duplicate_revision.contents = clone_contents
+      @duplicate_revision.ui_associations = clone_ui_associations
+      @duplicate_revision.slides = clone_slides
+      @duplicate_revision
     end
 
     def clone_contents
-      instance.contents.each do |content|
-        duplicate.contents << clone_content(content, duplicate)
-      end
-      duplicate
+      instance.current_revision.contents.map{|c| clone_content(c)}
     end
 
     def clone_ui_associations
-      instance.ui_associations.each do |ui_assoc|
-        new_ui_assoc = ui_assoc.dup
-        duplicate.ui_associations << new_ui_assoc
-      end
-      duplicate
+      instance.current_revision.ui_associations.map{|ui_assoc| ui_assoc.dup}
     end
 
     def clone_slides
-      instance.slides.each do |slide|
+      instance.current_revision.slides.map do |slide|
         new_slide = slide.dup
         slide.contents.each do |content|
-          clone_content(content, new_slide)
+          new_slide.contents << clone_content(content)
         end
-        duplicate.slides << new_slide
+        new_slide
       end
-      duplicate
     end
 
-    def clone_tags
-      duplicate.tag_list = instance.tag_list
-      duplicate
+    def clone_content(content)
+      Georgia::Content.new(
+        locale: content.locale,
+        text: content.text,
+        title: content.title,
+        excerpt: content.excerpt,
+        image_id: content.image_id,
+        keyword_list: content.keyword_list
+      )
     end
 
-    def clone_content(content, contentable)
-      new_content = content.dup
-      new_content.keyword_list = content.keyword_list
-      contentable.save unless contentable.persisted?
-      new_content.contentable = contentable
-      new_content.save
-      new_content
-    end
-
-    # alter the uuid to have an original uuid
-    def alter_uuid
-      duplicate.update_attribute(:uuid, UUIDTools::UUID.timestamp_create.to_s)
-    end
-
-    # alter the slug to have an original url
+    # alter the slug to add '-copy' at the end and keep url unique
     def alter_slug
-      duplicate.update_attribute(:slug, duplicate.slug + '-copy')
+      duplicate.slug = "#{instance.slug}-copy"
     end
 
     # alter the title to keep original title unique
     def alter_title
-      duplicate.contents.each do |content|
+      duplicate.current_revision.contents.each do |content|
         content.update_attribute(:title, "#{content.title} (Copy)")
       end
     end
