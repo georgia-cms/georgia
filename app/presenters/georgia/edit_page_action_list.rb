aba7631eea21b7126484ae8391eb0aa1631a436a
Mathieu Gagné <mathieu@motioneleven.com>
2013-08-08 11:39:02 -0400

    Fixed action list

--- app/presenters/georgia/edit_page_action_list.rb
+++ app/presenters/georgia/edit_page_action_list.rb
@@ -1,6 +1,16 @@
 module Georgia
   class EditPageActionList < EditRevisionActionList
 
+    def render
+      html = ""
+      html << content_tag(:li, link_to_draft) if can?(:draft, instance)
+      html << content_tag(:li, link_to_copy) if can?(:copy, instance)
+      html << content_tag(:li, link_to_publish) if can?(:publish, instance) and !instance.published?
+      html << content_tag(:li, link_to_unpublish) if can?(:unpublish, instance) and instance.published?
+      html << content_tag(:li, link_to_delete) if can?(:delete, instance)
+      html.html_safe
+    end
+
     private
 
     def page
@@ -8,11 +18,19 @@ module Georgia
     end
 
     def link_to_draft
-      link_to "#{icon_tag('icon-paste')} Start a new Draft".html_safe, url_for_page_action(:draft)
+      link_to "#{icon_tag('icon-paste')} Start a new Draft".html_safe, url_for_action(:draft)
     end
 
     def link_to_copy
-      link_to "#{icon_tag('icon-copy')} Copy".html_safe, url_for_page_action(:copy)
+      link_to "#{icon_tag('icon-copy')} Copy".html_safe, url_for_action(:copy)
+    end
+
+    def link_to_publish
+      link_to "#{icon_tag('icon-thumbs-up')} Publish".html_safe, url_for_action(:publish)
+    end
+
+    def link_to_unpublish
+      link_to "#{icon_tag('icon-thumbs-down')} Unpublish".html_safe, url_for_action(:unpublish), data: {confirm: 'Are you sure?'}
     end
 
     def link_to_delete
@@ -21,6 +39,10 @@ module Georgia
       options[:data][:confirm] = 'Are you sure?'
       options[:method] ||= :delete
       link_to "#{icon_tag('icon-trash')} Delete".html_safe, instance, options
+    end
+
+    def url_for_action action
+      url_for([action, page])
     end
 
   end
