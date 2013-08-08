368bf0f1fe79d8fa580da9d089cf7a8ab9a20a7e
Mathieu Gagné <mathieu@motioneleven.com>
2013-08-08 12:30:34 -0400

    Add color to page states

--- app/decorators/georgia/page_decorator.rb
+++ app/decorators/georgia/page_decorator.rb
@@ -2,10 +2,25 @@ module Georgia
   class PageDecorator < Georgia::ApplicationDecorator
     decorates_association :current_revision
 
-    delegate :status_tag, :excerpt_or_text, to: :current_revision
+    delegate :excerpt_or_text, to: :current_revision
 
     def template_path
       "pages/templates/#{current_revision.template}"
+    end
+
+    def state_icon
+      if public?
+        h.icon_tag('icon-ok icon-success')
+      else
+        h.icon_tag('icon-remove icon-failure')
+      end
+    end
+
+    def status_tag(options={})
+      options[:class] ||= ''
+      options[:class] << ' label'
+      options[:class] << " label-#{public? ? 'success' : 'warning'}"
+      h.content_tag(:span, (public? ? 'public' : 'private'), options)
     end
 
   end
