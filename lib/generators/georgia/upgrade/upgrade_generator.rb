b5d3d18b24c77d369f62ba59c0edfc990fc40b85
Mathieu Gagné <mathieu@motioneleven.com>
2013-08-08 17:22:01 -0400

    Remove revision_id migrations

--- lib/generators/georgia/upgrade/upgrade_generator.rb
+++ lib/generators/georgia/upgrade/upgrade_generator.rb
@@ -17,14 +17,14 @@ module Georgia
 
       def run_migrations
         migration_template "create_georgia_revisions.rb", "db/migrate/create_georgia_revisions.rb"
-        migration_template "add_revision_id_to_georgia_slides.rb", "db/migrate/add_revision_id_to_georgia_slides.rb"
-        migration_template "add_revision_id_to_georgia_ui_associations.rb", "db/migrate/add_revision_id_to_georgia_ui_associations.rb"
         migration_template "add_public_to_georgia_pages.rb", "db/migrate/add_public_to_georgia_pages.rb"
         rake 'db:migrate'
       end
 
       def run_upgrade_tasks
-        rake 'georgia:upgrade:migrate_to_revisions'
+        rake 'georgia:clean'
+        rake 'georgia:upgrade'
+        rake 'sunspot:reindex'
       end
 
     end
