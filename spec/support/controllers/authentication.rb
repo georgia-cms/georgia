c5347686ab63da80fb7aded0b82a329ab1c22d27
Mathieu Gagné <mathieu@motioneleven.com>
2013-08-12 14:20:39 -0400

    Add authentication flow helper

--- spec/support/controllers/authentication.rb
+++ spec/support/controllers/authentication.rb
@@ -9,6 +9,16 @@ module AuthenticationHelpers
   end
 
   def login(user)
-    login_as user, scope: :user
+    login_as user, scope: :georgia_user, run_callbacks: false
   end
+
+  def login_as_admin
+    visit '/admin/login'
+    admin = create(:admin)
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
+    click_button 'Log In'
+    page.should have_content admin.email
+  end
+
 end
