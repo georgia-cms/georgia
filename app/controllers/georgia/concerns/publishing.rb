c13851fc2c4de6c8b1e460660ff8e85a0563559c
Mathieu Gagné <mathieu@motioneleven.com>
2013-08-12 16:06:29 -0400

    Move publishing concerns methods inside included block

--- app/controllers/georgia/concerns/publishing.rb
+++ app/controllers/georgia/concerns/publishing.rb
@@ -6,26 +6,28 @@ module Georgia
       extend ActiveSupport::Concern
       include Helpers
 
-      def draft
-        @page = model.find(params[:id])
-        @draft = @page.draft
-        redirect_to [:edit, @page, @draft], notice: "You successfully started a new draft of #{@draft.title}. Submit for review when completed."
-      end
+      included do
+        def draft
+          @page = model.find(params[:id])
+          @draft = @page.draft
+          redirect_to [:edit, @page, @draft], notice: "You successfully started a new draft of #{@draft.title}. Submit for review when completed."
+        end
 
-      def publish
-        @page = model.find(params[:id])
-        @page.publish
-        message = "#{current_user.name} has successfully published #{@page.title} #{instance_name}."
-        notify(message)
-        redirect_to :back, notice: message
-      end
+        def publish
+          @page = model.find(params[:id])
+          @page.publish
+          message = "#{current_user.name} has successfully published #{@page.title} #{instance_name}."
+          notify(message)
+          redirect_to :back, notice: message
+        end
 
-      def unpublish
-        @page = model.find(params[:id])
-        @page.unpublish
-        message = "#{current_user.name} has successfully unpublished #{@page.title} #{instance_name}."
-        notify(message)
-        redirect_to :back, notice: message
+        def unpublish
+          @page = model.find(params[:id])
+          @page.unpublish
+          message = "#{current_user.name} has successfully unpublished #{@page.title} #{instance_name}."
+          notify(message)
+          redirect_to :back, notice: message
+        end
       end
 
     end
