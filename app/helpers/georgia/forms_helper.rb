6cbe16f4b8cc550a8afaa347d01aeb67f20f4e08
Mathieu Gagné <mathieu@motioneleven.com>
2013-08-12 16:04:02 -0400

    Fix parent_page_collection when no title

--- app/helpers/georgia/forms_helper.rb
+++ app/helpers/georgia/forms_helper.rb
@@ -17,7 +17,7 @@ module Georgia
     end
 
     def parent_page_collection
-      Georgia::Page.includes(current_revision: :contents).sort_by(&:title).map{|p| [p.title, p.id]}
+      Georgia::Page.not_self(@page).joins(current_revision: :contents).sort_by(&:title).map{|p| [p.title, p.id]}
     end
 
     def facets_inputs facets=[]
