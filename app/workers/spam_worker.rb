b668a565b5a334f4ba63f725e860ebc0086d9533
Mathieu Gagné <mathieu@motioneleven.com>
2013-08-16 17:33:45 -0400

    Send message even when spam for now

--- app/workers/spam_worker.rb
+++ app/workers/spam_worker.rb
@@ -5,7 +5,7 @@ class SpamWorker
     @message = Georgia::Message.find(message_id)
     @message.update_attributes(spam: @message.spam?, verified_at: Time.zone.now)
     # TODO: Move this to a seperate worker, possibly a single bulk daily email
-    Notifier.notify_info(@message).deliver unless @message.spam
+    Notifier.notify_info(@message).deliver
   end
 
 
