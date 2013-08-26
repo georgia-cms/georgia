a19d902505520c8b277107b090c10ed5f8fecaed
Mathieu Gagné <mathieu@motioneleven.com>
2013-08-26 16:54:03 -0400

    Fix phone and subject values when string is empty

--- app/decorators/georgia/message_decorator.rb
+++ app/decorators/georgia/message_decorator.rb
@@ -1,12 +1,16 @@
 module Georgia
   class MessageDecorator < Georgia::ApplicationDecorator
 
+    def phone_or_none
+      phone.present? ? phone : h.content_tag(:span, 'no phone', class: 'muted')
+    end
+
     def subject_truncated
-      h.truncate(model.subject, :length => 20)
+      subject.present? ? h.truncate(subject, length: 20) : h.content_tag(:span, 'no subject', class: 'muted')
     end
 
     def message_truncated
-      h.truncate(model.message, :length => 50)
+      h.truncate(message, length: 50)
     end
 
   end
