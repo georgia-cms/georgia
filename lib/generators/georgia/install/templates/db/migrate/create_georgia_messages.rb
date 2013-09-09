b6a49ef1be5c6b79d252c76b6bd1dcb4f0d703c7
Mathieu Gagné <mathieu@motioneleven.com>
2013-09-09 17:07:09 -0400

    Fix indentation

--- lib/generators/georgia/install/templates/db/migrate/create_georgia_messages.rb
+++ lib/generators/georgia/install/templates/db/migrate/create_georgia_messages.rb
@@ -2,18 +2,18 @@ class CreateGeorgiaMessages < ActiveRecord::Migration
 
   def change
     create_table :georgia_messages do |t|
-      t.string	 :name
-      t.string	 :email
-      t.string	 :phone
-      t.string	 :subject
-      t.string	 :attachment
-      t.text		 :message
-      t.boolean	 :spam, default: false
-			t.datetime :verified_at
-			t.string	 :permalink
-			t.string	 :user_ip
-			t.string	 :user_agent
-			t.string	 :referrer
+      t.string   :name
+      t.string   :email
+      t.string   :phone
+      t.string   :subject
+      t.string   :attachment
+      t.text     :message
+      t.boolean  :spam, default: false
+      t.datetime :verified_at
+      t.string   :permalink
+      t.string   :user_ip
+      t.string   :user_agent
+      t.string   :referrer
       t.timestamps
     end
   end
