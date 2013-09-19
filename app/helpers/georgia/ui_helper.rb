2cb603ff6234e6bd882a021f13232c42a6e090ee
Mathieu Gagné <mathieu@motioneleven.com>
2013-09-19 10:54:44 -0400

    Revamp revisions#edit layout

--- app/helpers/georgia/ui_helper.rb
+++ app/helpers/georgia/ui_helper.rb
@@ -7,5 +7,17 @@ module Georgia
       content_tag :i, nil, options
     end
 
+    def link_to_back
+      link_to icon_tag('level-up icon-rotate-270'), :back, class: 'btn-back'
+    end
+
+    def link_to_close
+      link_to "&times;".html_safe, "#", class: 'btn-close js-close'
+    end
+
+    def button_to_save
+      content_tag :button, "#{icon_tag 'ok'} Save".html_safe, type: "submit", class: "btn-save"
+    end
+
   end
 end
