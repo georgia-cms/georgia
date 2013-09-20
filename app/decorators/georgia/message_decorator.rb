47ce6b141e2ad8f075fbb32e8e107a0dbdabdcf5
Mathieu Gagné <mathieu@motioneleven.com>
2013-09-20 10:39:53 -0400

    Revamp messages board

--- app/decorators/georgia/message_decorator.rb
+++ app/decorators/georgia/message_decorator.rb
@@ -6,11 +6,11 @@ module Georgia
     end
 
     def subject_truncated
-      subject.present? ? h.truncate(subject, length: 20) : h.content_tag(:span, 'no subject', class: 'muted')
+      subject.present? ? h.truncate(subject, length: 60) : h.content_tag(:span, 'no subject', class: 'muted')
     end
 
     def message_truncated
-      h.truncate(message, length: 50)
+      h.truncate(message, length: 120)
     end
 
   end
