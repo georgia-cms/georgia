0c370eed00d41d725025785a4976bc904ca60f7d
Mathieu Gagné <mathieu@motioneleven.com>
2013-10-16 10:20:56 -0400

    Fixes #7 - Fight back UTF-8 Invalid Byte Sequences

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,13 +4,24 @@ class MessagesController < ApplicationController
   # Requires main application to have a messages/create.js.erb template and the form to be set remote: true
   # FIXME: Should handle multiple response formats and provides default templates
   def create
-    @message = Georgia::Message.new(params[:message])
-    @message.referrer = request.referrer
-    @message.user_ip = request.remote_ip
-    @message.user_agent = request.user_agent
+    @message = Georgia::Message.new(message_params)
     if @message.valid?
       SpamWorker.perform_async(@message.id) if @message.save
     end
     render layout: false
   end
+
+  private
+
+  def message_params
+    @message_params = {}
+    params[:message].each do |key, value|
+      @message_params[key] = value.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+    end
+    @message_params[:referrer] = request.referrer
+    @message_params[:user_ip] = request.remote_ip
+    @message_params[:user_agent] = request.user_agent
+    @message_params
+  end
+
 end
