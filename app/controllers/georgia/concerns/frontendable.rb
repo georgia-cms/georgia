110903656323ee21c8594320ffd3fddaa80e5f5e
Mathieu Gagné <mathieu@motioneleven.com>
2013-10-23 16:02:36 -0400

    Fix undefined_method cache_key when no page are found

--- app/controllers/georgia/concerns/frontendable.rb
+++ app/controllers/georgia/concerns/frontendable.rb
@@ -35,7 +35,7 @@ module Georgia
         end
 
         def page_cache_key
-          Georgia::Page.where(url: "/#{params[:request_path]}").first.cache_key
+          Georgia::Page.where(url: "/#{params[:request_path]}").try(:first).try(:cache_key)
         end
 
       end
