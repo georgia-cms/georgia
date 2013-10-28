d0765f3590b50623c1c20e1e6425d21f621c1778
Mathieu Gagné <mathieu@motioneleven.com>
2013-10-28 14:02:14 -0400

    Add sidebar_navigation_sublink

--- app/helpers/georgia/sidebar_helper.rb
+++ app/helpers/georgia/sidebar_helper.rb
@@ -3,10 +3,15 @@ module Georgia
 
     def sidebar_navigation_link text, url, options={}
       controller = options.fetch(:controller)
-      icon = options.fetch(:icon)
+      icon = options.fetch(:icon, 'bookmark-empty')
       content_tag :li, class: "#{'active' if controller_name == controller}" do
         link_to((content_tag(:span, icon_tag(icon), class: 'icon') + text), url)
       end
+    end
+
+    def sidebar_navigation_sublink text, url, options={}
+      controller = options.fetch(:controller)
+      content_tag :li, link_to(text, url), class: "#{'active' if controller_name == controller}"
     end
 
   end
