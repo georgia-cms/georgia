0f78c6d595272b1855081298c929fb32310a7321
Mathieu Gagné <mathieu@motioneleven.com>
2013-10-29 15:56:45 -0400

    Add white sidebar and empty message

--- app/helpers/georgia/pagination_helper.rb
+++ app/helpers/georgia/pagination_helper.rb
@@ -2,6 +2,7 @@ module Georgia
   module PaginationHelper
 
     def pagination_tag view, search
+      return unless search and !search.total.zero?
       Georgia::PaginationPresenter.new(view, search)
     end
 
--- app/helpers/georgia/sidebar_helper.rb
+++ app/helpers/georgia/sidebar_helper.rb
@@ -5,13 +5,31 @@ module Georgia
       controller = options.fetch(:controller)
       icon = options.fetch(:icon, 'bookmark-o')
       content_tag :li, class: "#{'active' if controller_name == controller}" do
-        link_to((content_tag(:span, icon_tag(icon), class: 'icon') + text), url)
+        link_to((sidebar_link_bundle(icon, text)), url)
       end
     end
 
     def sidebar_navigation_sublink text, url, options={}
       controller = options.fetch(:controller)
-      content_tag :li, link_to(text, url), class: "#{'active' if controller_name == controller}"
+      content_tag :li, link_to(sidebar_title_tag(text), url), class: "#{'active' if controller_name == controller}"
+    end
+
+    private
+
+    def sidebar_link_bundle icon, text
+      sidebar_title_icon(icon) + sidebar_title_tag(text) + sidebar_arrow_icon
+    end
+
+    def sidebar_title_icon icon
+      content_tag(:span, icon_tag(icon), class: 'icon')
+    end
+
+    def sidebar_arrow_icon
+      content_tag(:span, icon_tag('angle-left'), class: 'arrow')
+    end
+
+    def sidebar_title_tag text
+      content_tag(:span, text, class: 'title')
     end
 
   end
