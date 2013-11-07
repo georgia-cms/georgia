31492a6e9981d244b0158b2d47f7e4306fdb9d34
Mathieu Gagné <mathieu@motioneleven.com>
2013-11-07 16:04:46 -0500

    Fix pagination buttons to be disabled instead of hidden

--- app/presenters/georgia/pagination_presenter.rb
+++ app/presenters/georgia/pagination_presenter.rb
@@ -3,7 +3,7 @@ module Georgia
 
     attr_accessor :view_context
 
-    delegate :content_tag, :icon_tag, :link_to_previous_page, :link_to_next_page, to: :view_context
+    delegate :content_tag, :icon_tag, :link_to_previous_page, :link_to_next_page, :link_to, to: :view_context
 
     def initialize view_context, search
       @view_context = view_context
@@ -12,19 +12,46 @@ module Georgia
 
     def to_s
       content_tag(:div, class: 'header-pagination') do
-        content_tag(:span, search_count, class: 'pagination-count') +
-        link_to_previous_page(@search.hits, icon_tag('chevron-left')) +
-        link_to_next_page(@search.hits, icon_tag('chevron-right'))
+        search_count_tag + navigation_tags
       end
     end
 
     private
+
+    def search_count_tag
+      content_tag(:span, search_count, class: 'pagination-count')
+    end
+
+    def navigation_tags
+      output = ActiveSupport::SafeBuffer.new
+      output << previous_page
+      output << next_page
+      content_tag :div, output, class: 'btn-group'
+    end
 
     def search_count
       page = @search.results.offset+1
       count = @search.results.last_page? ? @search.total : @search.results.offset+@search.results.per_page
       total = @search.total
       "#{page} - #{count} of #{total}"
+    end
+
+    def previous_page
+      text = icon_tag('chevron-left')
+      link_to_previous_page(@search.hits, text, class: btn_class) || link_to_disabled(text)
+    end
+
+    def next_page
+      text = icon_tag('chevron-right')
+      link_to_next_page(@search.hits, text, class: btn_class, role: 'button') || link_to_disabled(text)
+    end
+
+    def btn_class
+      'btn btn-default'
+    end
+
+    def link_to_disabled text
+      link_to(text, '#', class: "#{btn_class} disabled", role: 'button')
     end
 
   end
