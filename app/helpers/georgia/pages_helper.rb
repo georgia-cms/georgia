971596eaa79b95721c85fb287b0a3ae93d1beca9
Mathieu Gagné <mathieu@motioneleven.com>
2014-01-09 11:58:17 -0500

    Fixes page#settings url editing

--- app/helpers/georgia/pages_helper.rb
+++ app/helpers/georgia/pages_helper.rb
@@ -10,5 +10,9 @@ module Georgia
       link_to icon_tag('cogs'), [:settings, @page], class: 'btn btn-default'
     end
 
+    def page_url_minus_slug
+      @page_full_url ||= (Georgia.url + @page.url).gsub(@page.slug, '')
+    end
+
   end
 end
