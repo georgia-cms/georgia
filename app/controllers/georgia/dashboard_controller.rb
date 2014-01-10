cc02ac43376ae6c0f8a26544c9a8f806a77c4499
Mathieu Gagné <mathieu@motioneleven.com>
2014-01-10 14:50:55 -0500

    Adds awaiting review revisions to dashboard

--- app/controllers/georgia/dashboard_controller.rb
+++ app/controllers/georgia/dashboard_controller.rb
@@ -2,6 +2,9 @@ module Georgia
   class DashboardController < Georgia::ApplicationController
 
     def show
+      if can?(:approve, Georgia::Revision)
+        @awaiting_revisions = Georgia::Revision.reviews.select{|r| r.revisionable.present?}
+      end
     end
 
   end
