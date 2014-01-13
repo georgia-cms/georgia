b88747aa2dece088318822b1ebd3a95e77b7567d
Mathieu Gagné <mathieu@motioneleven.com>
2014-01-13 12:42:33 -0500

    Fixes pagination_tag not to include view object in inputs

--- app/helpers/georgia/pagination_helper.rb
+++ app/helpers/georgia/pagination_helper.rb
@@ -1,9 +1,9 @@
 module Georgia
   module PaginationHelper
 
-    def pagination_tag view, search
+    def pagination_tag search
       return unless search and !search.total.zero?
-      Georgia::PaginationPresenter.new(view, search)
+      Georgia::PaginationPresenter.new(self, search)
     end
 
   end
