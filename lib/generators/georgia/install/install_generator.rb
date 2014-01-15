0cb4c2006747fec3abbc1d0a2c667014ef71c74f
Mathieu Gagné <mathieu@motioneleven.com>
2014-01-15 09:23:55 -0500

    Fixes install generator

--- lib/generators/georgia/install/install_generator.rb
+++ lib/generators/georgia/install/install_generator.rb
@@ -19,13 +19,9 @@ module Georgia
         # Must be in reverse order to keep priorities
         route "get '*request_path', to: 'pages#show', as: :page"
         route "root to: 'pages#show', request_path: 'home'"
-
         route "resources :messages, only: [:create]"
-        route "resources :pages, only: [] do
-    get :preview, on: :member
-  end"
-        route "mount Georgia::Engine => '/admin'
-  mount Ckeditor::Engine => '/ckeditor'\n"
+        route "mount Ckeditor::Engine => '/ckeditor'"
+        route "mount Georgia::Engine => '/admin'"
       end
 
       def self.next_migration_number(number)
@@ -40,10 +36,23 @@ module Georgia
       def copy_templates
         template "config/initializers/georgia.rb"
         template "app/controllers/pages_controller.rb"
+        template 'Procfile'
+      end
+
+      def add_dev_gems
+        gem_group :development do
+          gem "foreman"
+          gem "sunspot_solr", '2.0.0'
+        end
+        run "bundle"
       end
 
       def bootstrap
-        rake "georgia:install"
+        rake 'georgia:install'
+      end
+
+      def show_readme
+        readme "README"
       end
 
     end
