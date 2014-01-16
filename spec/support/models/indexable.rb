31dbf71d9e51dd188129d915f39423ef53bc4b39
Mathieu Gagné <mathieu@motioneleven.com>
2014-01-16 17:38:35 -0500

    Stubbing solr

--- spec/support/models/indexable.rb
+++ spec/support/models/indexable.rb
@@ -9,20 +9,21 @@ shared_examples "a indexable model" do
       described_class.should respond_to :search
     end
 
-    it 'in full text mode by query' do
-      revision = create(:georgia_revision)
-      revision.contents << build(:georgia_content, title: 'Wise Wiesel')
-      instance.revisions << revision
-      instance.current_revision = revision
-      instance.save
-      described_class.reindex
-      search = described_class.search do
-        fulltext 'Wise' do
-          fields(:title)
-        end
-      end
-      expect(search.results).to include(instance)
-    end
+    # Decided to stub Solr instead for faster test suite
+    # it 'in full text mode by query' do
+    #   revision = create(:georgia_revision)
+    #   revision.contents << build(:georgia_content, title: 'Wise Wiesel')
+    #   instance.revisions << revision
+    #   instance.current_revision = revision
+    #   instance.save
+    #   described_class.reindex
+    #   search = described_class.search do
+    #     fulltext 'Wise' do
+    #       fields(:title)
+    #     end
+    #   end
+    #   expect(search.results).to include(instance)
+    # end
 
   end
 
