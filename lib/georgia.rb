14ed01ebec54084d1890889b4b4d3ee80b94af2f
Mathieu Gagné <mathieu@motioneleven.com>
2014-01-21 15:29:04 -0500

    Depecrate Georgia.header to Georgia.navigation

--- lib/georgia.rb
+++ lib/georgia.rb
@@ -2,6 +2,7 @@ require "georgia/version"
 require "georgia/paths"
 require "georgia/engine"
 require "georgia/assets"
+require "georgia/indexer"
 
 module Georgia
 
@@ -14,11 +15,22 @@ module Georgia
   mattr_accessor :url
   @@url = "http://www.example.com"
 
-  mattr_accessor :header
-  @@header = %w(dashboard pages messages media navigation widgets)
+  mattr_accessor :navigation
+  @@navigation = %w(dashboard pages messages media navigation widgets)
+
+  mattr_accessor :indexer
+  @@indexer = Georgia::Indexer::Adapter::SolrAdapter.new()
 
   def self.setup
     yield self
+  end
+
+  class << self
+    alias :header :navigation
+
+    def header= value
+      ActiveSupport::Deprecation.warn("config.header is deprecated, use config.navigation instead.", caller)
+    end
   end
 
 end
