fa43a7cfc03215c77c5b1f27ad71a5473118075b
Mathieu Gagne <gagne.mathieu@hotmail.com>
2014-04-28 12:58:09 -0400

    Change indexer adapter for Travis env

--- spec/georgia/indexer_spec.rb
+++ spec/georgia/indexer_spec.rb
@@ -6,17 +6,18 @@ describe Georgia::Indexer do
   it {should respond_to :search }
   it {should respond_to :register_extension }
 
+  let(:adapter) {ENV['TRAVIS'] ? Georgia::Indexer::TireAdapter : Georgia::Indexer::SolrAdapter}
 
   describe '.adapter' do
     it 'defaults to Tire' do
-      expect(Georgia::Indexer.adapter).to be Georgia::Indexer::TireAdapter
+      expect(Georgia::Indexer.adapter).to be adapter
     end
   end
 
   describe '.search' do
     it 'delegates to adapter' do
-      expect(Georgia::Indexer::TireAdapter).to receive(:search)
-      Georgia::Indexer.search(nil, {})
+      expect(adapter).to receive(:search)
+      Georgia::Indexer.search(Georgia::Page, {})
     end
   end
 
