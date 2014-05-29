6f43676ed64d6f836d34146dd3c8b514a89ed638
Mathieu Gagné <mgagne@vigilantglobal.com>
2014-05-29 16:41:31 -0400

    Fixes media library

--- app/controllers/georgia/api/media_controller.rb
+++ app/controllers/georgia/api/media_controller.rb
@@ -3,7 +3,7 @@ module Georgia
     class MediaController < Georgia::ApplicationController
 
       def pictures
-        search_conditions = Georgia::PictureSearch.new(params).definition
+        search_conditions = Georgia::MediaSearch.new(params).definition
         @search = Ckeditor::Picture.search(search_conditions).page(params[:page]).per(params.fetch(:per, 12))
         @pictures = Ckeditor::PictureDecorator.decorate_collection(@search.records)
         render layout: false
