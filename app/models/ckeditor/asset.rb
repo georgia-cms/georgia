3a37ee81d5cd441318ea67555c1f90ee55b66664
Mathieu Gagné <mgagne@vigilantglobal.com>
2014-07-16 13:45:30 -0400

    ngram search on media library asset file names

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -51,4 +51,22 @@ class Ckeditor::Asset < ActiveRecord::Base
     '> 1 MB' => 1000..999999
   }
 
+  settings analysis: {
+    analyzer: {
+      'index_ngram_analyzer'  => {type: 'custom', tokenizer: 'standard', filter: ['standard', 'lowercase', 'my_ngram_filter']},
+      'search_analyzer'       => {type: 'custom', tokenizer: 'standard', filter: ['standard', 'lowercase']}
+    },
+    filter: {
+      'my_ngram_filter' => {type: 'nGram', min_gram: 2, max_gram: 10, token_chars: [ "letter", "digit", "whitespace", "punctuation", "symbol" ]}
+    }
+  } do
+    mapping index_analyzer: 'index_ngram_analyzer', search_analyzer: 'search_analyzer'
+  end
+
+  def as_indexed_json options={}
+    self.as_json(
+      only: [:id, :data_file_name, :updated_at, :type],
+      methods: [:tag_list]
+    )
+  end
 end
--- app/searches/georgia/media_search.rb
+++ app/searches/georgia/media_search.rb
@@ -1,5 +1,26 @@
 module Georgia
   class MediaSearch < SearchDefinition
 
+    def process
+      add_fulltext_query_filter if query.present?
+      add_default_sorting unless query.present?
+    end
+
+    private
+
+    def default_definition
+      {
+        highlight: { fields: { data_file_name: {type: "plain"} } }
+      }
+    end
+
+    def add_default_sorting
+      @definition[:sort] = { updated_at: :desc }
+    end
+
+    def add_fulltext_query_filter
+      @definition[:query] = {multi_match: {query: query, fields: ['data_file_name', 'tag_list']}}
+    end
+
   end
 end
