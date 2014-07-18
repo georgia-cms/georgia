8e25fbff37cb7b0fac3f53a986b0258098eb22f4
Mathieu Gagné <mgagne@vigilantglobal.com>
2014-07-18 11:33:33 -0400

    Namespaced GeorgiaMailer to Georgia::Mailer

--- app/controllers/georgia/dashboard_controller.rb
+++ app/controllers/georgia/dashboard_controller.rb
@@ -5,10 +5,8 @@ module Georgia
       authorize Georgia::Dashboard
       @activities = PublicActivity::Activity.order(created_at: :desc).page(params[:page]).per(20)
       @awaiting_revisions = policy_scope(Georgia::Revision)
-      if defined? GeorgiaMailer::Message
-        # if can?(:index, GeorgiaMailer::Message)
-          @messages = GeorgiaMailer::Message.ham.latest.limit(5).decorate
-        # end
+      if defined? Georgia::Mailer::Message and policy(Georgia::Mailer::Message).index?
+        @messages = Georgia::Mailer::Message.ham.latest.limit(5).decorate
       end
     end
 
