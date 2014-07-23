1867bf54a24bc272c924b3af5a13be973606999f
Mathieu Gagné <mgagne@vigilantglobal.com>
2014-07-23 09:09:43 -0400

    Awaiting revisions should only be of state review

--- app/controllers/georgia/dashboard_controller.rb
+++ app/controllers/georgia/dashboard_controller.rb
@@ -4,7 +4,7 @@ module Georgia
     def show
       authorize Georgia::Dashboard
       @activities = PublicActivity::Activity.order(created_at: :desc).page(params[:page]).per(20)
-      @awaiting_revisions = policy_scope(Georgia::Revision)
+      @awaiting_revisions = Georgia::Revision.where("status = ?", Georgia::Revision.statuses[:review])
       if defined? Georgia::Mailer::Message and policy(Georgia::Mailer::Message).index?
         @messages = Georgia::Mailer::Message.ham.latest.limit(5).decorate
       end
